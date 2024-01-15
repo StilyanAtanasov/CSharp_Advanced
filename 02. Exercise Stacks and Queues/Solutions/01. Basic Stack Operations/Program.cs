4ad3bc3c82b068669f71662466a23f47f42dc745
Stilyan Atanasov <stilyan2008@gmail.com>
2024-01-15 19:56:33 +0200

    exercise 1 upload

---
+++ 02. Exercise Stacks and Queues/Solutions/01. Basic Stack Operations/Program.cs
@@ -0,0 +1,14 @@
+﻿int[] values = Console.ReadLine()!.Split().Select(int.Parse).ToArray();
+
+int n = values[0]; // Stack length
+int s = values[1]; // Elements to Pop()
+int x = values[2]; // Element to find
+
+Stack<int> stack = new(Console.ReadLine()!.Split().Select(int.Parse));
+
+if (s > n) s = n;
+for (int i = 0; i < s; i++) stack.Pop();
+
+if (stack.Count == 0) Console.WriteLine(0);
+else if (stack.Contains(x)) Console.WriteLine("true");
+else Console.WriteLine(stack.Min());
