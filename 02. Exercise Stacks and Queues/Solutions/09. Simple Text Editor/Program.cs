f0edf8230a9f89c12fa7e1abcdf7c03bf137779a
Stilyan Atanasov <stilyan2008@gmail.com>
2024-01-16 20:23:21 +0200

    Added Solutions

---
+++ 02. Exercise Stacks and Queues/Solutions/09. Simple Text Editor/Program.cs
@@ -0,0 +1,29 @@
+﻿Stack<string> textHistory = new();
+
+ushort operationsCount = ushort.Parse(Console.ReadLine()!);
+for (int i = 0; i < operationsCount; i++)
+{
+    string command = Console.ReadLine()!;
+    if (textHistory.Count == 0)
+    {
+        if (command[0] == '1') textHistory.Push(command[2..]);
+        continue;
+    }
+
+    string current = textHistory.Peek();
+    switch (command[0])
+    {    
+        case '1':
+            textHistory.Push(current + command[2..]);
+            break;
+        case '2':
+            textHistory.Push(current[..^int.Parse(command[2..])]);
+            break;
+        case '3':
+            Console.WriteLine(current[int.Parse(command[2..]) - 1]);
+            break;
+        case '4':
+            textHistory.Pop();
+            break;
+    }
+}
