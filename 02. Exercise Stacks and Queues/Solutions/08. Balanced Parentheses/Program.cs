8e6bc6e5b61f21af6e7f236308a4483b5e52a33e
Stilyan Atanasov <stilyan2008@gmail.com>
2024-01-16 18:33:00 +0200

    Added solutions

---
+++ 02. Exercise Stacks and Queues/Solutions/08. Balanced Parentheses/Program.cs
@@ -0,0 +1,28 @@
+﻿Stack<char> parentheses = new();
+
+string inputParentheses = Console.ReadLine()!;
+if (inputParentheses.Length % 2 == 1)
+{
+    Console.WriteLine("NO");
+    return;
+}
+
+foreach (char c in inputParentheses)
+{
+    char currentChar = c;
+    if (currentChar == '(' || currentChar == '[' || currentChar == '{') parentheses.Push(currentChar);
+    else
+    {
+        char correctChar = '\0';
+        switch (parentheses.Peek())
+        {
+            case '(': correctChar = ')'; break;
+            case '[': correctChar = ']'; break;
+            case '{': correctChar = '}'; break;
+        }
+
+        if (c == correctChar) parentheses.Pop();
+    }
+}
+
+Console.WriteLine(parentheses.Count == 0 ? "YES" : "NO");
