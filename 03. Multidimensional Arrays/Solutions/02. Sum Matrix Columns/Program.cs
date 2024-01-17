1ca539516d868fcea73098341727149b3b8471a1
Stilyan Atanasov <stilyan2008@gmail.com>
2024-01-17 20:37:54 +0200

    Added Solutions

--- 03. Multidimensional Arrays/Solutions/02. Sum Matrix Columns/Program.cs
+++ 03. Multidimensional Arrays/Solutions/02. Sum Matrix Columns/Program.cs
@@ -1,2 +1,10 @@
 ﻿uint[] matrixSize = Console.ReadLine()!.Split(", ").Select(uint.Parse).ToArray();
-int[,] matrix = new int[matrixSize[0], matrixSize[1]];
+int[] sumByColumns = new int[matrixSize[1]];
+
+for (int i = 0; i < matrixSize[0]; i++)
+{
+    int[] row = Console.ReadLine()!.Split(" ").Select(int.Parse).ToArray();
+    for (int j = 0; j < matrixSize[1]; j++) sumByColumns[j] += row[j];
+}
+
+Console.WriteLine(string.Join("\n", sumByColumns));
