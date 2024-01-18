d5be57adc2c5846ebd5b138983aad2232204e2b1
Stilyan Atanasov <stilyan2008@gmail.com>
2024-01-18 15:43:17 +0200

    Added Solutions

---
+++ 04. Exercise Multidimensional Arrays/Solutions/01. Diagonal Difference/Program.cs
@@ -0,0 +1,18 @@
+﻿ushort squareMatrixSize = ushort.Parse(Console.ReadLine()!);
+int[,] squareMatrix = new int[squareMatrixSize, squareMatrixSize];
+
+for (int i = 0; i < squareMatrixSize; i++)
+{
+    int[] row = Console.ReadLine()!.Split().Select(int.Parse).ToArray();
+    for (int j = 0; j < squareMatrixSize; j++) squareMatrix[i, j] = row[j];
+}
+
+int primaryDiagonalSum = 0;
+int secondaryDiagonalSum = 0;
+for (int i = 0; i < squareMatrixSize; i++)
+{
+    primaryDiagonalSum += squareMatrix[i, i];
+    secondaryDiagonalSum += squareMatrix[i, squareMatrixSize - i - 1];
+}
+
+Console.WriteLine(Math.Abs(primaryDiagonalSum - secondaryDiagonalSum));
