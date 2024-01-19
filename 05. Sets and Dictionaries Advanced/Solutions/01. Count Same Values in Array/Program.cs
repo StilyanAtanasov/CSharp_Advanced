d0db5161f72cb75e8942b61cf80f9e1fa3da145a
Stilyan Atanasov <stilyan2008@gmail.com>
2024-01-19 22:22:26 +0200

    Solution Added

---
+++ 05. Sets and Dictionaries Advanced/Solutions/01. Count Same Values in Array/Program.cs
@@ -0,0 +1,6 @@
+﻿Dictionary<double, uint> numbers = new();
+
+double[] inputNumbers = Console.ReadLine()!.Split().Select(double.Parse).ToArray();
+foreach (double number in inputNumbers) numbers[number] = numbers.TryGetValue(number, out uint number1) ? number1 + 1 : 1;
+
+foreach (var number in numbers) Console.WriteLine($"{number.Key} - {number.Value} times");
