1c84840564d23a531cfc5c49aa8df36cb00203ff
Stilyan Atanasov <stilyan2008@gmail.com>
2024-01-19 17:17:10 +0200

    Upload Solution

---
+++ 04. Exercise Multidimensional Arrays/Solutions/6. Jagged Array Manipulator/Program.cs
@@ -0,0 +1,30 @@
+﻿ushort rows = ushort.Parse(Console.ReadLine()!);
+decimal[][] jaggedArray = new decimal[rows][];
+
+for (int i = 0; i < rows; i++) jaggedArray[i] = Console.ReadLine()!.Split().Select(decimal.Parse).ToArray();
+
+for (int i = 0; i < rows - 1; i++)
+{
+    if (jaggedArray[i].Length != jaggedArray[i + 1].Length) for (int k = 0; k < 2; k++) for (int j = 0; j < jaggedArray[i + k].Length; j++) jaggedArray[i + k][j] /= 2;
+    else for (int k = 0; k < 2; k++) for (int j = 0; j < jaggedArray[i + k].Length; j++) jaggedArray[i + k][j] *= 2;
+}
+
+string command = Console.ReadLine()!;
+while (command != "End")
+{
+    string[] operation = command.Split();
+    int rowIndex = int.Parse(operation[1]);
+    int columnIndex = int.Parse(operation[2]);
+    int value = int.Parse(operation[3]);
+
+    if (rowIndex < rows && rowIndex >= 0 && columnIndex >= 0 && columnIndex < jaggedArray[rowIndex].Length) 
+        switch (operation[0])
+        {
+            case "Add": jaggedArray[rowIndex][columnIndex] += value; break;
+            case "Subtract": jaggedArray[rowIndex][columnIndex] -= value; break;
+        }
+
+    command = Console.ReadLine()!;
+}
+
+foreach (decimal[] array in jaggedArray) Console.WriteLine(string.Join(" ", array));
