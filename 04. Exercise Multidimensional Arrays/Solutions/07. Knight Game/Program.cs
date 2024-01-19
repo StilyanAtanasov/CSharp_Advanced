1f62faf4f83fece1a74a23066b020c87f48a18cc
Stilyan Atanasov <stilyan2008@gmail.com>
2024-01-19 19:52:47 +0200

    Update Solutions

--- 04. Exercise Multidimensional Arrays/Solutions/07. Knight Game/Program.cs
+++ 04. Exercise Multidimensional Arrays/Solutions/07. Knight Game/Program.cs
@@ -10,11 +10,11 @@ for (byte i = 0; i < squareMatrixSize; i++)
 ushort knightsRemoved = 0;
 for (int max = 8; max > 0; max--)
 {
-    Dictionary<string, ushort> knights = new(); // string => "knightRow, knightColumn"; int => number of attacks received;
     for (int i = 0; i < squareMatrixSize; i++)
     {
         for (int j = 0; j < squareMatrixSize; j++)
         {
+            byte endangered = 0;
             if (squareMatrix[i, j] == 'K')
             {
                 string[] moves =
@@ -25,29 +25,19 @@ for (int max = 8; max > 0; max--)
 
                 foreach (string move in moves)
                 {
-                    int y = int.Parse(move.Split()[1]);
-                    int x = int.Parse(move.Split()[0]);
-                    if (y >= 0 && y < squareMatrixSize && x >= 0 && x < squareMatrixSize && squareMatrix[y, x] == 'K')
-                    {
-                        if (knights.ContainsKey($"{y} {x}")) knights[$"{y} {x}"]++;
-                        else knights.Add($"{y} {x}", 1);
-                    }
-
+                    int y = int.Parse(move.Split()[0]);
+                    int x = int.Parse(move.Split()[1]);
+                    if (y >= 0 && y < squareMatrixSize && x >= 0 && x < squareMatrixSize && squareMatrix[y, x] == 'K') endangered++;
                 }
+            }
+
+            if (endangered == max)
+            {
+                squareMatrix[i, j] = '0';
+                knightsRemoved++;
             }
         }
     }
-
-    var orderedKnights = knights.OrderByDescending(x => x.Value);
-    var firstKnight = orderedKnights.FirstOrDefault();
-
-    if (firstKnight.Value == max)
-    {
-        string knightToRemove = firstKnight.Key;
-        squareMatrix[int.Parse(knightToRemove.Split()[0]), int.Parse(knightToRemove.Split()[1])] = '0';
-        knightsRemoved++;
-    }
-
 }
 
 Console.WriteLine(knightsRemoved);
