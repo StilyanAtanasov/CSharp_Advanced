b7e06980ddf23fd0a93853564e32affdef16af13
Stilyan Atanasov <stilyan2008@gmail.com>
2024-01-20 09:02:43 +0200

    Updated Solution

--- 05. Sets and Dictionaries Advanced/Solutions/02. Average Student Grades/Program.cs
+++ 05. Sets and Dictionaries Advanced/Solutions/02. Average Student Grades/Program.cs
@@ -1,1 +1,14 @@
-﻿
+﻿Dictionary<string, List<decimal>> students = new();
+
+byte numberOfStudents = byte.Parse(Console.ReadLine()!);
+for (int i = 0; i < numberOfStudents; i++)
+{
+    string[] studentInfo = Console.ReadLine()!.Split();
+    string studentName = studentInfo[0];
+    decimal studentGrade = decimal.Parse(studentInfo[1]);
+
+    if (students.TryGetValue(studentName, out var student)) student.Add(studentGrade);
+    else students[studentName] = new List<decimal> {studentGrade};
+}
+
+foreach (var student in students) Console.WriteLine($"{student.Key} -> {string.Join(" ", student.Value.Select(s => s.ToString("F2")))} (avg: {student.Value.Average():F2})");
