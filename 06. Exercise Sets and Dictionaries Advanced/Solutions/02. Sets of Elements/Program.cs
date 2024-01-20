534c744cfafc76a010e54cd437768b1cb7dbfc8e
Stilyan Atanasov <stilyan2008@gmail.com>
2024-01-20 15:37:54 +0200

    Added Solutions

---
+++ 06. Exercise Sets and Dictionaries Advanced/Solutions/02. Sets of Elements/Program.cs
@@ -0,0 +1,12 @@
+﻿byte[] sizes = Console.ReadLine()!.Split().Select(byte.Parse).ToArray();
+byte n = sizes[0]; // First HashSet Length
+byte m = sizes[1]; // Second HashSet Length
+
+HashSet<decimal> setN = new();
+HashSet<decimal> setM = new();
+
+for (int i = 0; i < n; i++) setN.Add(decimal.Parse(Console.ReadLine()!));
+for (int i = 0; i < m; i++) setM.Add(decimal.Parse(Console.ReadLine()!));
+
+setN.IntersectWith(setM);
+Console.WriteLine(string.Join(" ", setN));
---
+++ 06. Exercise Sets and Dictionaries Advanced/Solutions/03. Periodic Table/Program.cs
@@ -0,0 +1,4 @@
+﻿HashSet<string> uniqueElements = new();
+for (int i = 0; i < byte.Parse(Console.ReadLine()!); i++) 
+    foreach (string element in Console.ReadLine()!.Split()) uniqueElements.Add(element); // The input is a name
+Console.WriteLine(string.Join(" ", uniqueElements));
