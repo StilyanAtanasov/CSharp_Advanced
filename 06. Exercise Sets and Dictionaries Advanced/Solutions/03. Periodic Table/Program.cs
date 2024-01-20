c750e0cc9c21bae717a483295e9af83f63dd9a8a
Stilyan Atanasov <stilyan2008@gmail.com>
2024-01-20 15:42:52 +0200

    Updated Solutions

--- 06. Exercise Sets and Dictionaries Advanced/Solutions/03. Periodic Table/Program.cs
+++ 06. Exercise Sets and Dictionaries Advanced/Solutions/03. Periodic Table/Program.cs
@@ -1,4 +1,6 @@
-﻿HashSet<string> uniqueElements = new();
-for (int i = 0; i < byte.Parse(Console.ReadLine()!); i++) 
-    foreach (string element in Console.ReadLine()!.Split()) uniqueElements.Add(element); // The input is a name
+﻿SortedSet<string> uniqueElements = new();
+
+byte rowsCount = byte.Parse(Console.ReadLine()!);
+for (int i = 0; i < rowsCount ; i++) foreach (string element in Console.ReadLine()!.Split()) uniqueElements.Add(element); // The input is a name
+
 Console.WriteLine(string.Join(" ", uniqueElements));
