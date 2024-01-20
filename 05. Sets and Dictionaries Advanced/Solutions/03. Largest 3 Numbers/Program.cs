a7cb591ec454c05e9495579c3ea570f7eca71209
Stilyan Atanasov <stilyan2008@gmail.com>
2024-01-20 10:06:58 +0200

    Added Solutions

---
+++ 05. Sets and Dictionaries Advanced/Solutions/03. Largest 3 Numbers/Program.cs
@@ -0,0 +1,2 @@
+﻿List<int> integers = Console.ReadLine()!.Split().Select(int.Parse).OrderByDescending(x => x).Take(3).ToList();
+ Console.Write(string.Join(" ", integers));
---
+++ 05. Sets and Dictionaries Advanced/Solutions/04. Product Shop/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Globalization;
+
+SortedDictionary<string, Dictionary<string, double>> shops = new();
+
+string command = Console.ReadLine()!;
+while (command != "Revision")
+{
+    string[] productInfo = command.Split(", ");
+    string shop = productInfo[0];
+    string product = productInfo[1];
+    double price = double.Parse(productInfo[2]);
+
+    if (!shops.ContainsKey(shop)) shops[shop] = new Dictionary<string, double>();
+    shops[shop].Add(product, price);
+
+    command = Console.ReadLine()!;
+}
+
+foreach (var (shopName, products) in shops)
+{
+    Console.WriteLine($"{shopName}->");
+    foreach (var (productName, productPrice) in products) Console.WriteLine($"Product: {productName}, Price: {productPrice}");
+}
