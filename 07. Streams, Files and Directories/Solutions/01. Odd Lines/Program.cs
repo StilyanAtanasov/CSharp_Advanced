e3ff0897b996ba7c90a6ad699c4fda1c1fc83615
Stilyan Atanasov <stilyan2008@gmail.com>
2024-01-23 17:53:22 +0200

    Added Solution

---
+++ 07. Streams, Files and Directories/Solutions/01. Odd Lines/Program.cs
@@ -0,0 +1,18 @@
+﻿string inputFilePath = @"..\..\..\Files\input.txt";
+string outputFilePath = @"..\..\..\Files\output.txt";
+
+ExtractOddLines(inputFilePath, outputFilePath);
+
+static void ExtractOddLines(string inputFilePath, string outputFilePath)
+{
+    using StreamReader reader = new StreamReader(inputFilePath);
+    using StreamWriter writer = new StreamWriter(outputFilePath);
+    ushort counter = 0;
+    string? line = reader.ReadLine();
+    while (line != null)
+    {
+        if (counter % 2 == 1) writer.WriteLine(line);
+        counter++;
+        line = reader.ReadLine();
+    }
+}
