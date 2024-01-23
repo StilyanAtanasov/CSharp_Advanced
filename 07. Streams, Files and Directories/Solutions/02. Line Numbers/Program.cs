9da9d2db25b528158d3ccab09c7ffc1160d3b775
Stilyan Atanasov <stilyan2008@gmail.com>
2024-01-23 18:19:38 +0200

    Added Solution

---
+++ 07. Streams, Files and Directories/Solutions/02. Line Numbers/Program.cs
@@ -0,0 +1,13 @@
+﻿string inputPath = @"..\..\..\Files\input.txt";
+string outputPath = @"..\..\..\Files\output.txt";
+
+RewriteFileWithLineNumbers(inputPath, outputPath);
+
+static void RewriteFileWithLineNumbers(string inputFilePath, string outputFilePath)
+{
+    using StreamReader reader = new StreamReader(inputFilePath);
+    using StreamWriter writer = new StreamWriter(outputFilePath);
+
+    ushort index = 1;
+    while (!reader.EndOfStream) writer.WriteLine($"{index++}. {reader.ReadLine()}");
+}
