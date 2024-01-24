13d20c46659168722436475a348d07b88fc38460
Stilyan Atanasov <stilyan2008@gmail.com>
2024-01-24 21:13:21 +0200

    Added Solution

--- 07. Streams, Files and Directories/Solutions/05. Extract Special Bytes/Program.cs
+++ 07. Streams, Files and Directories/Solutions/05. Extract Special Bytes/Program.cs
@@ -6,4 +6,20 @@ ExtractBytesFromBinaryFile(binaryFilePath, bytesFilePath, outputPath);
 
 static void ExtractBytesFromBinaryFile(string binaryFilePath, string bytesFilePath, string outputPath)
 {
+    using FileStream binaryFile = new FileStream(binaryFilePath, FileMode.Open);
+    using StreamReader bytesFile = new StreamReader(bytesFilePath);
+    using StreamWriter writer = new StreamWriter(outputPath);
+
+    List<byte> bytes = new List<byte>();
+    while (!bytesFile.EndOfStream) bytes.Add(byte.Parse(bytesFile.ReadLine()!));
+    
+    byte[] buffer = new byte[1000];
+    while (true)
+    {
+        if (binaryFile.Read(buffer) == 0) break;
+
+        foreach (byte b in bytes) if (buffer.Contains(b)) writer.WriteLine(b);
+
+        binaryFile.Write(buffer, 0, buffer.Length);
+    }
 }
