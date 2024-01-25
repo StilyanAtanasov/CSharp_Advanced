6d166b20bff56aaec5c87cbb4c1bb613946b87b2
Stilyan Atanasov <stilyan2008@gmail.com>
2024-01-25 16:14:01 +0200

    Added Solution

---
+++ 07. Streams, Files and Directories/Solutions/07. Folder Size/Program.cs
@@ -0,0 +1,16 @@
+﻿string folderPath = @"..\..\..\Files\TestFolder";
+string outputPath = @"..\..\..\Files\output.txt";
+
+GetFolderSize(folderPath, outputPath);
+
+static void GetFolderSize(string folderPath, string outputFilePath)
+{
+    DirectoryInfo folderInfo = new(folderPath);
+    FileInfo[] files = folderInfo.GetFiles("*", SearchOption.AllDirectories);
+
+    ulong sum = 0;
+    foreach (FileInfo file in files) sum += (ulong)file.Length;
+
+    using StreamWriter writer = new(outputFilePath);
+    writer.Write(sum / 1000.0 + " KB");
+}
