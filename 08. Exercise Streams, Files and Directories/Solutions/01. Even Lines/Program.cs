1a6b3f84753faccb091ee1d89b6603f8657dbe86
Stilyan Atanasov <stilyan2008@gmail.com>
2024-01-25 22:23:55 +0200

    Added Solution

---
+++ 08. Exercise Streams, Files and Directories/Solutions/01. Even Lines/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Text;
+
+string inputFilePath = @"..\..\..\text.txt";
+Console.WriteLine(ProcessLines(inputFilePath));
+
+static string ProcessLines(string inputFilePath)
+{
+    List<string[]> lines = new();
+
+    using StreamReader reader = new(inputFilePath);
+    string? currentLine = reader.ReadLine();
+    uint counter = 0;
+    while (currentLine != null)
+    {
+        if (counter % 2 == 0) lines.Add(currentLine.Split());
+        currentLine = reader.ReadLine();
+        counter++;
+    }
+
+    char[] specialSymbols = { '-', ',', '.', '!', '?' };
+    foreach (var line in lines)
+    {
+        for (int j = 0; j < line.Length; j++) foreach (char specialSymbol in specialSymbols) if (line[j].Contains(specialSymbol)) line[j] = line[j].Replace(specialSymbol, '@');
+        Array.Reverse(line);
+    }
+
+    StringBuilder sb = new();
+    foreach (string[] line in lines) sb.Append(string.Join(" ", line) + '\n');
+
+    return sb.ToString();
+}
---
+++ 08. Exercise Streams, Files and Directories/Solutions/02. Line Numbers/Program.cs
@@ -0,0 +1,30 @@
+﻿string inputFilePath = @"..\..\..\text.txt";
+string outputFilePath = @"..\..\..\output.txt";
+
+ProcessLines(inputFilePath, outputFilePath);
+
+static void ProcessLines(string inputFilePath, string outputFilePath)
+{
+    File.WriteAllText(outputFilePath, "");
+
+    string[] lines = File.ReadAllLines(inputFilePath);
+
+    char[] punctuationMarks = { '-', ',', '.', '!', '?', '\'', '"' };
+    for (int i = 0; i < lines.Length; i++)
+    {
+        string temp = lines[i];
+        ulong punctuationMarksCount = 0;
+
+        foreach (char punctuationMark in punctuationMarks)
+        {
+            ulong tempLength = (ulong) temp.Length;
+            temp = temp.Replace(Convert.ToString(punctuationMark), "");
+            punctuationMarksCount += tempLength - (ulong) temp.Length;
+        }
+
+        temp = temp.Replace(" ", "");
+        uint lettersCount = (uint)temp.Length;
+
+        File.AppendAllText(outputFilePath, $"Line {i + 1}: {lines[i]} ({lettersCount})({punctuationMarksCount})" + '\n');
+    }
+}
