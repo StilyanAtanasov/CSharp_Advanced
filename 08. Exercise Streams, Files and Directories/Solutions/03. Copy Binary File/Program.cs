1607e325f756c3cc20ecaa9a61fe3197af461483
Stilyan Atanasov <stilyan2008@gmail.com>
2024-01-25 22:48:16 +0200

    Solution Added

---
+++ 08. Exercise Streams, Files and Directories/Solutions/03. Copy Binary File/Program.cs
@@ -0,0 +1,13 @@
+﻿string inputFilePath = @"..\..\..\copyMe.png";
+string outputFilePath = @"..\..\..\copyMe-copy.png";
+
+CopyFile(inputFilePath, outputFilePath);
+
+static void CopyFile(string inputFilePath, string outputFilePath)
+{
+    using FileStream inputFile = new(inputFilePath, FileMode.Open);
+    using FileStream outputFile = new(outputFilePath, FileMode.Create);
+
+    byte[] buffer = new byte[1024];
+    while (inputFile.Read(buffer) != 0) outputFile.Write(buffer);
+}
