8fd39d1fc4df9f6cbabc4db0a8253dd982bf2ba3
Stilyan Atanasov <stilyan2008@gmail.com>
2024-01-26 17:28:11 +0200

    Solutions Added

---
+++ 08. Exercise Streams, Files and Directories/Solutions/05. Copy Directory/Program.cs
@@ -0,0 +1,17 @@
+﻿string inputPath = @$"{Console.ReadLine()!}";
+string outputPath = @$"{Console.ReadLine()!}";
+
+CopyAllFiles(inputPath, outputPath);
+
+static void CopyAllFiles(string inputPath, string outputPath)
+{
+    if (Directory.Exists(outputPath)) Directory.Delete(outputPath, true);
+    Directory.CreateDirectory(outputPath);
+
+    string[] files = Directory.GetFiles(inputPath);
+    foreach (var file in files)
+    {
+        FileInfo fileInfo = new(file);
+        File.Copy(file, outputPath + '\\' + fileInfo.Name);
+    }
+}
---
+++ 08. Exercise Streams, Files and Directories/Solutions/06. Zip and Extracts/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.IO.Compression;
+
+string inputFile = @"..\..\..\copyMe.png";
+string zipArchiveFile = @"..\..\..\archive.zip";
+string extractedFile = @"..\..\..\extracted.png";
+
+ZipFileToArchive(inputFile, zipArchiveFile);
+
+var fileNameOnly = Path.GetFileName(inputFile);
+ExtractFileFromArchive(zipArchiveFile, fileNameOnly, extractedFile);
+
+
+static void ZipFileToArchive(string inputFilePath, string zipArchiveFilePath)
+{
+   using ZipArchive archive = ZipFile.Open(zipArchiveFilePath, ZipArchiveMode.Create);
+   archive.CreateEntryFromFile(inputFilePath, Path.GetFileName(inputFilePath));
+}
+
+static void ExtractFileFromArchive(string zipArchiveFilePath, string fileName, string outputFilePath)
+{
+    using ZipArchive archive = ZipFile.Open(zipArchiveFilePath, ZipArchiveMode.Read);
+    archive.GetEntry(fileName)!.ExtractToFile(outputFilePath);
+}
