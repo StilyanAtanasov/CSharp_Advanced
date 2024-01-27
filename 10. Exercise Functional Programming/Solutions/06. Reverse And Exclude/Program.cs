c9e3f373ad84d4930f57eaeb610edd162270f5a4
Stilyan Atanasov <stilyan2008@gmail.com>
2024-01-27 18:46:02 +0200

    Solution Added

---
+++ 10. Exercise Functional Programming/Solutions/06. Reverse And Exclude/Program.cs
@@ -0,0 +1,6 @@
+﻿int[] numbers = Console.ReadLine()!.Split().Select(int.Parse).Reverse().ToArray();
+int divider = int.Parse(Console.ReadLine()!);
+
+// ReSharper disable once ConvertToLocalFunction
+Predicate<int> isMultiple = multiple => multiple % divider != 0;
+Console.WriteLine(string.Join(' ', numbers.Where(x => isMultiple(x))));
