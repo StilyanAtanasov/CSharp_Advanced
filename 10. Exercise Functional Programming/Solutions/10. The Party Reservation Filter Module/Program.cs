92b67abb6a53cfddd92d10a224243b01623ba062
Stilyan Atanasov <stilyan2008@gmail.com>
2024-01-28 13:18:52 +0200

    Solutions Added

---
+++ 10. Exercise Functional Programming/Solutions/10. The Party Reservation Filter Module/Program.cs
@@ -0,0 +1,43 @@
+﻿List<string> people = Console.ReadLine()!.Split().ToList();
+
+Dictionary<string, Predicate<string>> filters = new();
+string command = Console.ReadLine()!;
+while (command != "Print")
+{
+    Predicate<string> filter = null!;
+
+    string[] operation = command.Split(';');
+    string modifier = operation[0];
+    string key = operation[1];
+    string value = operation[2];
+    switch (key)
+    {
+        case "Starts with":
+            filter = s => s[..value.Length] == value;
+            break;
+        case "Ends with":
+            filter = s => s[^value.Length..] == value;
+            break;
+        case "Length":
+            filter = s => s.Length == int.Parse(value);
+            break;
+        case "Contains":
+            filter = s => s.Contains(value);
+            break;
+    }
+
+    switch (modifier)
+    {
+        case "Add filter":
+            filters.Add(key + value, filter);
+            break;
+        case "Remove filter":
+            filters.Remove(key + value);
+            break;
+    }
+
+    command = Console.ReadLine()!;
+}
+
+foreach (var filter in filters) people.RemoveAll(filter.Value);
+Console.WriteLine(string.Join(' ', people));
---
+++ 10. Exercise Functional Programming/Solutions/11. TriFunction/Program.cs
@@ -0,0 +1,8 @@
+﻿// ReSharper disable ConvertToLocalFunction
+int minSum = int.Parse(Console.ReadLine()!);
+string[] people = Console.ReadLine()!.Split();
+
+Func<string, int, bool> getSum = (name, min) => name.Sum(c => c) >= min;
+Func<string[], int, Func<string, int, bool>, string> getFirstName = (names, min, checker) => names.First(x => checker(x, min));
+
+Console.WriteLine(getFirstName(people, minSum, getSum));
