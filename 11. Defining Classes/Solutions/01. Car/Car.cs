72382a96eefdec84c8ae9a5ff1a8b6f1ef47bf16
Stilyan Atanasov <stilyan2008@gmail.com>
2024-01-29 21:47:59 +0200

    Solution Added

---
+++ 11. Defining Classes/Solutions/01. Car/Car.cs
@@ -0,0 +1,28 @@
+﻿// ReSharper disable All
+namespace CarManufacturer;
+
+class Car
+{
+    private string make;
+    private string model;
+    private int year;
+
+
+    public string Make
+    {
+        get => make;
+        set => make = value;
+    }
+
+    public string Model
+    {
+        get => model;
+        set => model = value;
+    }
+    public int Year
+    {
+        get => year;
+        set => year = value;
+    }
+
+}
---
+++ 11. Defining Classes/Solutions/01. Car/Program.cs
@@ -0,0 +1,18 @@
+﻿// ReSharper disable CheckNamespace
+namespace CarManufacturer;
+
+public class StartUp
+{
+    static void Main()
+    {
+        // Example Usage
+        Car car = new()
+        {
+            Make = "VW",
+            Model = "MK3",
+            Year = 1992
+        };
+
+        Console.WriteLine($"Make: {car.Make} \nModel: {car.Model} \nYear: {car.Year}");
+    }
+}
