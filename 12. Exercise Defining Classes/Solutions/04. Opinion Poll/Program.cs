096dd8c29e1ff2985a0feefe5e24f266ca46cf10
Stilyan Atanasov <stilyan2008@gmail.com>
2024-02-01 20:51:13 +0200

    Solution Added

---
+++ 12. Exercise Defining Classes/Solutions/04. Opinion Poll/Program.cs
@@ -0,0 +1,22 @@
+﻿// ReSharper disable once CheckNamespace
+namespace DefiningClasses;
+
+public class StartUp
+{
+    public static void Main()
+    {
+        List<Person> people = new();
+
+        ushort familyMembers = ushort.Parse(Console.ReadLine()!);
+        for (int i = 0; i < familyMembers; i++)
+        {
+            string[] member = Console.ReadLine()!.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            string name = member[0];
+            int age = int.Parse(member[1]);
+
+            people.Add(new Person(name, age));
+        }
+
+        foreach (Person person in people.Where(p => p.Age > 30).OrderBy(p => p.Name)) Console.WriteLine($"{person.Name} - {person.Age}");
+    }
+}
