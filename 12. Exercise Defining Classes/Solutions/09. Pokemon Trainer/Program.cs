3b10826d56893128bb9f32eb15dc3ebc5ba483da
Stilyan Atanasov <stilyan2008@gmail.com>
2024-02-02 19:34:01 +0200

    Solution Added

---
+++ 12. Exercise Defining Classes/Solutions/09. Pokemon Trainer/Program.cs
@@ -0,0 +1,47 @@
+﻿using _09._Pokemon_Trainer;
+
+List<Trainer> trainers = new();
+
+string command;
+while ((command = Console.ReadLine()!) != "Tournament")
+{
+    string[] pokemonInfo = command.Split();
+
+    string trainerName = pokemonInfo[0];
+    string pokemonName = pokemonInfo[1];
+    string pokemonElement = pokemonInfo[2];
+    int pokemonHealth = int.Parse(pokemonInfo[3]);
+
+    if (trainers.FirstOrDefault(t => t.Name == trainerName) == null)
+    {
+        Trainer trainer = new Trainer(trainerName);
+        trainer.PokemonCollection.Add(new Pokemon(pokemonName, pokemonElement, pokemonHealth));
+        trainers.Add(trainer);
+    }
+    else trainers.First(t => t.Name == trainerName).PokemonCollection.Add(new Pokemon(pokemonName, pokemonElement, pokemonHealth));
+}
+
+while ((command = Console.ReadLine()!) != "End")
+{
+    string element = command;
+
+    foreach (Trainer trainer in trainers)
+    {
+        if (trainer.PokemonCollection.FirstOrDefault(p => p.Element == element) != null) trainer.BadgesCount++;
+        else
+        {
+            for (int j = 0; j < trainer.PokemonCollection.Count; j++)
+            {
+                Pokemon pokemon = trainer.PokemonCollection[j];
+                pokemon.Health -= 10;
+                if (pokemon.Health <= 0)
+                {
+                    trainer.PokemonCollection.Remove(pokemon);
+                    j--;
+                }
+            }
+        }
+    }
+}
+
+foreach (Trainer trainer in trainers.OrderByDescending(t => t.BadgesCount)) Console.WriteLine($"{trainer.Name} {trainer.BadgesCount} {trainer.PokemonCollection.Count}");
---
+++ 12. Exercise Defining Classes/Solutions/09. Pokemon Trainer/Trainer.cs
@@ -0,0 +1,10 @@
+﻿namespace _09._Pokemon_Trainer;
+
+public class Trainer
+{
+    public Trainer(string name) => Name = name;
+
+    public string Name { get; set; }
+    public ushort BadgesCount { get; set; }
+    public List<Pokemon> PokemonCollection { get; set; } = new();
+}
