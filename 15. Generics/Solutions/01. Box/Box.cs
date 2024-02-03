46a55b0e6c5b48742a9adf1b140f528946eaf459
Stilyan Atanasov <stilyan2008@gmail.com>
2024-02-03 18:19:30 +0200

    Solution Added

---
+++ 15. Generics/Solutions/01. Box/Box.cs
@@ -0,0 +1,13 @@
+﻿// ReSharper disable once CheckNamespace
+namespace BoxOfT;
+
+public class Box<T>
+{
+
+    // ReSharper disable once FieldCanBeMadeReadOnly.Local
+    private Stack<T> _list = new();
+    public int Count => _list.Count;
+
+    public void Add(T item) => _list.Push(item);
+    public T Remove() => _list.Pop();
+}
---
+++ 15. Generics/Solutions/01. Box/Program.cs
@@ -0,0 +1,14 @@
+﻿// ReSharper disable once CheckNamespace
+using BoxOfT;
+
+// Example Usage
+Box<int> box = new Box<int>();
+box.Add(1);
+box.Add(2);
+box.Add(3);
+Console.WriteLine(box.Count);
+Console.WriteLine(box.Remove());
+box.Add(4);
+box.Add(5);
+Console.WriteLine(box.Remove());
+Console.WriteLine(box.Count);
