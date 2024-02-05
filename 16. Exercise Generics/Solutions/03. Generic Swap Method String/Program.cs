d0aeac26dae5dd418da77f0ae1c40d2226b12b76
Stilyan Atanasov <stilyan2008@gmail.com>
2024-02-05 10:58:37 +0200

    Added Solutions

---
+++ 16. Exercise Generics/Solutions/03. Generic Swap Method String/Program.cs
@@ -0,0 +1,16 @@
+﻿List<string> list = new();
+
+ushort inputLinesCount = ushort.Parse(Console.ReadLine()!);
+for (int i = 0; i < inputLinesCount; i++) list.Add(Console.ReadLine()!);
+
+int[] indexes = Console.ReadLine()!.Split().Select(int.Parse).ToArray();
+int index1 = indexes[0];
+int index2 = indexes[1];
+
+foreach (string s in Swapper(list, index1, index2)) Console.WriteLine($"{s.GetType()}: {s}");
+
+static List<T> Swapper<T>(List<T> list, int index1, int index2)
+{
+     (list[index1], list[index2]) = (list[index2], list[index1]);
+     return list;
+}
---
+++ 16. Exercise Generics/Solutions/04. Generic Swap Method Integer/Program.cs
@@ -0,0 +1,16 @@
+﻿List<int> list = new();
+
+ushort inputLinesCount = ushort.Parse(Console.ReadLine()!);
+for (int i = 0; i < inputLinesCount; i++) list.Add(int.Parse(Console.ReadLine()!));
+
+int[] indexes = Console.ReadLine()!.Split().Select(int.Parse).ToArray();
+int index1 = indexes[0];
+int index2 = indexes[1];
+
+foreach (int s in Swapper(list, index1, index2)) Console.WriteLine($"{s.GetType()}: {s}");
+
+static List<T> Swapper<T>(List<T> list, int index1, int index2)
+{
+    (list[index1], list[index2]) = (list[index2], list[index1]);
+    return list;
+}
