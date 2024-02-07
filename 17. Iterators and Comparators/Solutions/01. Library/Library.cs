d64c59d2e7fa79f878311ccd9e811991226740fc
Stilyan Atanasov <stilyan2008@gmail.com>
2024-02-07 21:44:00 +0200

    Added Solutions

---
+++ 17. Iterators and Comparators/Solutions/01. Library/Library.cs
@@ -0,0 +1,14 @@
+﻿// ReSharper disable FieldCanBeMadeReadOnly.Local
+using System.Collections;
+
+namespace IteratorsAndComparators;
+
+public class Library : IEnumerable<Book>
+{
+    private List<Book> _books;
+
+    public Library(params Book[] books) => this._books = books.ToList();
+    public IEnumerator<Book> GetEnumerator() => _books.GetEnumerator();
+
+    IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+}
