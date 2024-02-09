c84931d5a0d4ec8c6626f17972f6f06082076824
Stilyan Atanasov <stilyan2008@gmail.com>
2024-02-09 20:21:48 +0200

    Added Solution

---
+++ 18. Exercise Iterators and Comparators/Solutions/01. ListyIterator/ListyIterator.cs
@@ -0,0 +1,20 @@
+﻿namespace _01._ListyIterator;
+
+public class ListyIterator<T>
+{
+    private readonly List<T> _list;
+    private int _index;
+
+    public ListyIterator(params T[] list) => _list = new List<T>(list);
+
+    public bool HasNext() => _index + 1 < _list.Count;
+
+    public bool Move()
+    {
+        bool isValidOperation = HasNext();
+        if (isValidOperation) _index++;
+        return isValidOperation;
+    }
+
+    public void Print() => Console.WriteLine(_index < _list.Count ? _list[_index] : "Invalid Operation!");
+}
---
+++ 18. Exercise Iterators and Comparators/Solutions/01. ListyIterator/Program.cs
@@ -0,0 +1,14 @@
+﻿using _01._ListyIterator;
+
+string command = Console.ReadLine()!;
+ListyIterator<string> iterator = new(command.Split()[1..]); // Format: Create E1 E2 ...
+
+while ((command = Console.ReadLine()!) != "END")
+{
+    switch (command)
+    {
+        case "HasNext": Console.WriteLine(iterator.HasNext()); break;
+        case "Move": Console.WriteLine(iterator.Move()); break;
+        case "Print": iterator.Print(); break;
+    }
+}
