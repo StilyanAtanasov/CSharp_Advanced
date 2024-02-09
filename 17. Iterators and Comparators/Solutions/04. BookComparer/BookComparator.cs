0120754df90bf2883752cc941de45dbe33e414ac
Stilyan Atanasov <stilyan2008@gmail.com>
2024-02-09 19:53:33 +0200

    Added Solution

---
+++ 17. Iterators and Comparators/Solutions/04. BookComparer/BookComparator.cs
@@ -0,0 +1,11 @@
+﻿// ReSharper disable StringCompareToIsCultureSpecific
+namespace IteratorsAndComparators;
+public class BookComparator : IComparer<Book>
+{
+    public int Compare(Book? x, Book? y)
+    {
+        int result = x!.Title.CompareTo(y!.Title);
+        if (result == 0) result = y.Year.CompareTo(x.Year);
+        return result;
+    }
+}
