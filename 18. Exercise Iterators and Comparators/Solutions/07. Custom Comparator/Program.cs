680f0c9ae8851d41e0e702283498b621436d28b0
Stilyan Atanasov <stilyan2008@gmail.com>
2024-02-10 16:03:03 +0200

    Added Solutions

---
+++ 18. Exercise Iterators and Comparators/Solutions/07. Custom Comparator/Program.cs
@@ -0,0 +1,15 @@
+﻿int[] array = Console.ReadLine()!.Split().Select(int.Parse).ToArray();
+
+Array.Sort(array, new CustomComparator());
+Console.WriteLine(string.Join(' ', array));
+
+
+public class CustomComparator : IComparer<int>
+{
+    public int Compare(int x, int y)
+    {
+        if (x % 2 == 0 && y % 2 != 0) return -1;
+        if (x % 2 != 0 && y % 2 == 0) return 1;
+        return x < y ? -1 : 1;
+    }
+}
