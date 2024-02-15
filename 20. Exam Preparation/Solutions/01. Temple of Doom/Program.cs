30998adcc054673a77d57ab8b5726cf994419ac1
Stilyan Atanasov <stilyan2008@gmail.com>
2024-02-15 17:53:48 +0200

    Added Exam Preparation Solutions

---
+++ 20. Exam Preparation/Solutions/01. Temple of Doom/Program.cs
@@ -0,0 +1,38 @@
+﻿// Read the input
+Queue<uint> tools = new(Console.ReadLine()!.Split().Select(uint.Parse));
+Stack<uint> substances = new(Console.ReadLine()!.Split().Select(uint.Parse));
+List<uint> challenges = Console.ReadLine()!.Split().Select(uint.Parse).ToList();
+
+while (true)
+{
+    uint result = tools.Peek() * substances.Peek();
+    if (challenges.Contains(result))
+    {
+        tools.Dequeue();
+        substances.Pop();
+        challenges.RemoveAt(challenges.IndexOf(result));
+    }
+    else
+    {
+        tools.Enqueue(tools.Dequeue() + 1);
+        substances.Push(substances.Pop() - 1);
+        if (substances.Peek() == 0) substances.Pop();
+    }
+
+    if ((tools.Count == 0 || substances.Count == 0) && challenges.Count > 0)
+    {
+        Console.WriteLine("Harry is lost in the temple. Oblivion awaits him.");
+        break;
+    }
+
+    if (challenges.Count == 0)
+    {
+        Console.WriteLine("Harry found an ostracon, which is dated to the 6th century BCE.");
+        break;
+    }
+}
+
+// Print the output
+if (tools.Count > 0) Console.WriteLine($"Tools: {string.Join(", ", tools)}");
+if (substances.Count > 0) Console.WriteLine($"Substances: {string.Join(", ", substances)}");
+if (challenges.Count > 0) Console.WriteLine($"Challenges: {string.Join(", ", challenges)}");
---
+++ 20. Exam Preparation/Solutions/02. Mouse In The Kitchen/Program.cs
@@ -0,0 +1,136 @@
+﻿// Read the dimensions of the cupboard
+int[] cupboardDimensions = Console.ReadLine()!.Split(',').Select(int.Parse).ToArray();
+int rows = cupboardDimensions[0];
+int columns = cupboardDimensions[1];
+
+// Initialise and read the cupboard (matrix), find and set the mouse initial position
+char[,] cupboard = new char[rows, columns];
+int mouseRow = 0;
+int mouseColumn = 0;
+
+for (int i = 0; i < rows; i++)
+{
+    char[] row = Console.ReadLine()!.ToCharArray();
+    for (int j = 0; j < columns; j++)
+    {
+        if ((cupboard[i, j] = row[j]) == 'M')
+        {
+            mouseRow = i;
+            mouseColumn = j;
+        }
+    }
+}
+
+// Implement the movements
+string command;
+while ((command = Console.ReadLine()!) != "danger")
+{
+    if (command == "up")
+    {
+        if (CanMove(mouseRow - 1, rows - 1, 0)) // @ means wall
+        {
+            if (cupboard[mouseRow - 1, mouseColumn] != '@')
+            {
+                if (!CheckForTrap(mouseRow - 1, mouseColumn)) return;
+                cupboard[mouseRow, mouseColumn] = '*';
+                cupboard[mouseRow -= 1, mouseColumn] = 'M';
+            }
+        }
+        else return;
+
+        if (!CheckForCheese()) return;
+    }
+    else if (command == "down")
+    {
+        if (CanMove(mouseRow + 1, rows - 1, 0)) // @ means wall
+        {
+            if (cupboard[mouseRow + 1, mouseColumn] != '@')
+            {
+                if (!CheckForTrap(mouseRow + 1, mouseColumn)) return;
+                cupboard[mouseRow, mouseColumn] = '*';
+                cupboard[mouseRow += 1, mouseColumn] = 'M';
+            }
+        }
+        else return;
+
+        if (!CheckForCheese()) return;
+    }
+    else if (command == "right")
+    {
+        if (CanMove(mouseColumn + 1, columns - 1, 0)) // @ means wall
+        {
+            if (cupboard[mouseRow, mouseColumn + 1] != '@')
+            {
+                if (!CheckForTrap(mouseRow, mouseColumn + 1)) return;
+                cupboard[mouseRow, mouseColumn] = '*';
+                cupboard[mouseRow, mouseColumn += 1] = 'M';
+            }
+        }
+        else return;
+
+        if (!CheckForCheese()) return;
+    }
+    else // left
+    {
+        if (CanMove(mouseColumn - 1, columns - 1, 0))
+        {
+            if (cupboard[mouseRow, mouseColumn - 1] != '@')
+            {
+                if (!CheckForTrap(mouseRow, mouseColumn - 1)) return;
+                cupboard[mouseRow, mouseColumn] = '*';
+                cupboard[mouseRow, mouseColumn -= 1] = 'M';
+            }
+        }
+        else return;
+
+        if (!CheckForCheese()) return;
+    }
+}
+
+// Print the final result
+Console.WriteLine("Mouse will come back later");
+PrintMatrix();
+
+bool CanMove(int nextPosition, int maxBorder, int minBorder)
+{
+    if (nextPosition < minBorder || nextPosition > maxBorder)
+    {
+        Console.WriteLine("No more cheese for tonight!"); // The mouse has left the cupboard
+        PrintMatrix();
+        return false;
+    }
+
+    return true;
+}
+
+bool CheckForTrap(int row, int column)
+{
+    if (cupboard[row, column] == 'T') // T stands for trap
+    {
+        Console.WriteLine("Mouse is trapped!");
+        cupboard[mouseRow, mouseColumn] = '*';
+        cupboard[row, column] = 'M';
+        PrintMatrix();
+        return false;
+    }
+
+    return true;
+}
+
+bool CheckForCheese()
+{
+    foreach (char c in cupboard) if (c == 'C') return true; // C equals Cheese
+
+    Console.WriteLine("Happy mouse! All the cheese is eaten, good night!");
+    PrintMatrix();
+    return false;
+}
+
+void PrintMatrix()
+{
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++) Console.Write(cupboard[i, j]);
+        Console.WriteLine();
+    }
+}
