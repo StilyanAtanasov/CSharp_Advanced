5b1cff3544e54fa410f217ad3aa513260bfa9d29
Stilyan Atanasov <stilyan2008@gmail.com>
2024-04-12 19:13:01 +0300

    Added Solutions for Stacks and Queues - Exercise

---
+++ 02. Exercise Stacks and Queues/Solutions/11. Key Revolver/Program.cs
@@ -0,0 +1,38 @@
+﻿byte bulletPrice = byte.Parse(Console.ReadLine()!);
+ushort gunBarrelSize = ushort.Parse(Console.ReadLine()!);
+Stack<byte> bullets = new(Console.ReadLine()!.Split().Select(byte.Parse));
+Queue<byte> locks = new(Console.ReadLine()!.Split().Select(byte.Parse));
+uint intelligenceValue = uint.Parse(Console.ReadLine()!);
+
+ushort expenses = 0;
+ushort shootsLeft = gunBarrelSize;
+while (true)
+{
+    byte currentBullet = bullets.Pop();
+    byte currentLock = locks.Peek();
+    if (currentLock >= currentBullet)
+    {
+        Console.WriteLine("Bang!");
+        locks.Dequeue();
+    }
+    else Console.WriteLine("Ping!");
+
+    shootsLeft--;
+    expenses += bulletPrice;
+
+    if (shootsLeft == 0 && bullets.Count > 0)
+    {
+        Console.WriteLine("Reloading!");
+        shootsLeft = gunBarrelSize;
+    }
+    if (locks.Count == 0)
+    {
+        Console.WriteLine($"{bullets.Count} bullets left. Earned ${intelligenceValue - expenses}");
+        return;
+    }
+    if (bullets.Count == 0)
+    {
+        Console.WriteLine($"Couldn't get through. Locks left: {locks.Count}");
+        return;
+    }
+}
---
+++ 02. Exercise Stacks and Queues/Solutions/12. Cups and Bottles/Program.cs
@@ -0,0 +1,23 @@
+﻿Queue<ushort> cups = new(Console.ReadLine()!.Split().Select(ushort.Parse));
+Stack<ushort> bottles = new(Console.ReadLine()!.Split().Select(ushort.Parse));
+
+ushort wastedWater = 0;
+int currentCupCapacity = cups.Peek();
+while (true)
+{
+    ushort currentBottle = bottles.Pop();
+    currentCupCapacity -= currentBottle;
+    if (currentCupCapacity <= 0)
+    {
+        wastedWater -= (ushort)currentCupCapacity;
+        cups.Dequeue();
+        if (cups.Count != 0) currentCupCapacity = cups.Peek();
+    }
+
+    if (cups.Count == 0 || bottles.Count == 0)
+    {
+        Console.WriteLine($"{(cups.Count == 0 ? "Bottles" : "Cups")}: {(bottles.Count == 0 ? string.Join(' ', cups) : string.Join(' ', bottles))}");
+        Console.WriteLine($"Wasted litters of water: {wastedWater}");
+        return;
+    }
+}
